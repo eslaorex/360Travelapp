2333031da10e1266cf906fae777670ed3c44cf67
Dingyxx <38100464+Dingyxx@users.noreply.github.com>
2018-04-06 13:29:16 +0200

    antondd
    121232321

---
+++ 360app/360AppGroup01/Assets/Scripts/BlueColoredChanges.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueColoredChanges: MonoBehaviour {
+
+    public Material noLooky;
+    public Material eyeSpy;
+
+    void Start () {
+        GetComponent<Renderer>().material = noLooky;
+	}
+	
+	public void TotallyWatching()
+    {
+        GetComponent<Renderer>().material = eyeSpy;
+    }
+
+    public void NotEvenLooking()
+    {
+        GetComponent<Renderer>().material = noLooky;
+    }
+}
---
+++ 360app/360AppGroup01/Assets/Scripts/LoadScene2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene2 : MonoBehaviour
+{
+
+    void Start()
+    {
+        SceneManager.LoadScene("Project3");
+    }
+}
+
