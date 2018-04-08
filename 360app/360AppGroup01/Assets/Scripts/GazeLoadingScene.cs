7e843cd4ff574640aadc247285a9f6eda81a0b79
Eslao rex <eslao_rex@yahoo.com>
2018-04-08 22:32:02 +0200

    changes

--- 360app/360AppGroup01/Assets/Scripts/GazeLoadingScene.cs
+++ 360app/360AppGroup01/Assets/Scripts/GazeLoadingScene.cs
@@ -9,42 +9,44 @@ public class GazeLoadingScene : MonoBehaviour {
     public float myTime = 0f;
     public Transform RadialProgress;
     //public Transform nextSphere;
-    private bool gazeDat;
+  
 
     public float gazeTime = 3f;
-    void Start () {
-        RadialProgress.GetComponent<Image>().fillAmount = myTime;
+
+    void Start ()
+    {
+        //RadialProgress.GetComponent<Image>().fillAmount = myTime / 3; 
     }
 		
 	public void Update ()
     {
-        if (gazeDat)
+        
             myTime += Time.deltaTime;
             RadialProgress.GetComponent<Image>().fillAmount = myTime / 3;
 
-        if (myTime >= 3f)
+        if (myTime >= gazeTime)
         {
-           ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
+        
+            ExecuteEvents.Execute(gameObject, new PointerEventData(EventSystem.current), ExecuteEvents.pointerDownHandler);
            
-           PointerDown();          
         }
     }
     
     public void PointerEnter()
     {
-        gazeDat = true;
+
     }
 
     public void PointerExit()
     {
-        gazeDat = false;
+
         myTime = 0f;
         RadialProgress.GetComponent<Image>().fillAmount = myTime / 3;
     }
 
     public void PointerDown()
     {
-        Debug.Log("CHANGE SPHERE");
+   
     
     }
 
--- 360app/360AppGroup01/Assets/Scripts/SphereChangerNoFade.cs
+++ 360app/360AppGroup01/Assets/Scripts/SphereChangerNoFade.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class SphereChangerNoFade : MonoBehaviour {
 
+    //This ensures that we don't mash to change spheres
+    //bool changing = false;
 
 
-  
+    public void ChangeSphere(Transform nextSphere)
+    {
+        Camera.main.transform.parent.position = nextSphere.position;
+        //Start the fading process
 
+    }
+
+    
 
 }
